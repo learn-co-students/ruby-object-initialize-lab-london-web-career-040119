148a268c7586d868b77601289e1b7e721cbe15ba
Dwake5 <wakeling101@hotmail.com>
2019-04-03 12:50:08 +0100

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Person
+  def
+    initialize(name)
+      @name = name
+  end
+end
+
+# breed="X", sets default value to X
+class Dog
+  def
+    initialize(name, breed='Mutt')
+      @name = name
+      @breed = breed
+  end
+end
